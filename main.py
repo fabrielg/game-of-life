1c6bfec475cf0592f636bd6e51091410bd64c93d
Fabrielg <gfrancois2003@gmail.com>
2023-07-21 23:39:48 +0200

    feat: add slider

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import time
 import pygame
 import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 import numpy as np
 
 COLOR_BG = (0, 0, 0)
 COLOR_GRID = (40, 40, 40)
 COLOR_DIE_NEXT = (170, 170, 170)
 COLOR_ALIVE_NEXT = (255, 255, 255)
+SETTINGS_MENU_SIZE = 100
 
 
 def update(scren, cells, size, with_progress=False):
@@ -29,14 +32,18 @@ def update(scren, cells, size, with_progress=False):
                 update_cells[row, col] = 1
                 if with_progress:
                     color = COLOR_ALIVE_NEXT
-
-        pygame.draw.rect(scren, color, (col * size, row * size, size - 1, size - 1))
+        pygame.draw.rect(scren, color, (col * size, row * size + SETTINGS_MENU_SIZE, size - 1, size - 1))
 
     return update_cells
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
+
+    slider = Slider(screen, 50, 50, 100, 15, min=0.001, max=0.1, step=0.001, initial=0.001, handleRadius=12)
+    textbox = TextBox(screen, 175, 36, 50, 50, fontSize=20, borderColour=(255, 0, 0), textColour=(255, 0, 0), radius=10)
+
+    textbox.disable()
 
     cells = np.zeros((60, 80))
     screen.fill(COLOR_GRID)
@@ -46,12 +53,22 @@ def main():
     pygame.display.update()
 
     running = False
+    init = True
 
     while True:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        textbox.setText(str(round(slider.getValue(), 3)))
+        pygame_widgets.update(events)
+
+        if init:
+            pygame.display.update()
+            init = False
+
+        for event in events:
             if event.type == pygame.QUIT:
                 return
-            elif event.type == pygame.KEYDOWN:
+            
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = not running
                     update(screen, cells, 10)
@@ -59,7 +76,9 @@ def main():
 
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                cells[pos[1] // 10, pos[0] // 10] = 1
+                if pos[1] < 40:
+                    continue
+                cells[(pos[1] - SETTINGS_MENU_SIZE) // 10, pos[0] // 10] = 1
                 update(screen, cells, 10)
                 pygame.display.update()
 
@@ -69,7 +88,7 @@ def main():
             cells = update(screen, cells, 10, with_progress=True)
             pygame.display.update()
 
-        time.sleep(0.01)
+        time.sleep(slider.getValue())
 
 if __name__ == '__main__':
     main()
